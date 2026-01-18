f64d61cad8a7d9f5795648dd4c621091a043f63b
mattia.filosa <mattia.filosa@hibo.it>
2026-01-18 12:20:04 +0100

    feat: Add Zod schema validation and enhanced error handling for CSV product parsing, ensuring stream closure.

--- backend/src/features/products/services/csv-service.ts
+++ backend/src/features/products/services/csv-service.ts
@@ -1,25 +1,41 @@
 import fs from 'fs';
 import csv from 'csv-parser';
+import { createProductSchema } from '../product.schema.ts';
 
 export const parseProductsFromCsv = (filePath: string): Promise<any[]> => {
   return new Promise((resolve, reject) => {
     const results: any[] = [];
 
-    fs.createReadStream(filePath)
+    const stream = fs.createReadStream(filePath)
       .pipe(csv())
       .on('data', (data) => {
-        const formattedProduct = {
+        // map and validate data
+        const formattedProduct = createProductSchema.safeParse({
           name: data.name,
           description: data.description,
           price: parseFloat(data.price),
           discountPrice: data.discountPrice ? parseFloat(data.discountPrice) : null,
+        })
+
+        // if validation fails reject
+        if (!formattedProduct.success) {
+          // block read stream
+          stream.destroy();
+          console.error("Invalid product data:", formattedProduct.error.format());
+          reject(formattedProduct.error.format());
         }
-        results.push(formattedProduct);
+
+        results.push(formattedProduct.data);
       })
       .on('end', () => {
+        // close stream
+        stream.destroy();
         resolve(results);
       })
       .on('error', (error) => {
+        // close stream
+        stream.destroy();
+        console.error("Error parsing CSV:", error);
         reject(error);
       });
   });
